d09ea750c985a0d2a076502b2569f97a1fd031c4
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-10-31 09:27:27 -0600

    added files and psudo outline

---
+++ app/calc.js
@@ -0,0 +1,5 @@
+
+// We are going to need to take the user-input (total of bill)
+// and store it. Once we have that, based off the user total
+// We will then calculate the percent of the check and return
+// the amount for the user
