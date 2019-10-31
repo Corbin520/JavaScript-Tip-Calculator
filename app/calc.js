30de224593b9f5a646381cff14be3cad4054b8ad
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-10-31 10:38:51 -0600

    added options and js

--- app/calc.js
+++ app/calc.js
@@ -3,3 +3,31 @@
 // and store it. Once we have that, based off the user total
 // We will then calculate the percent of the check and return
 // the amount for the user
+
+
+var billTotal = []
+var tipPercent = []
+
+// get the value off the input price box
+$("#calculate-total").on("click", function() {
+    
+    var inpVal = $("#price-input").val().trim()
+    var tipVal = $("#tip-percentage").val().trim()
+
+    billTotal.push(inpVal)
+    tipPercent.push(tipVal)
+
+
+    
+    // we may have to use a switch statment based off the value clicked on the drop down.
+    
+    // so use this code: when the user picks the value of the drop down.
+    // excellent = .30%
+    // Great = .20%
+    // Good = .15%
+    // Terriable = .5%
+    
+
+})
+
+// Leave a review or see reviews
