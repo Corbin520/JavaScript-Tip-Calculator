c809494b7946b71eb8b248af8022413f2771dd65
Corbin Brockbank <corbinbrckbnk76@gmail.com>
2019-10-31 11:05:18 -0600

    added card and calc is working

--- app/calc.js
+++ app/calc.js
@@ -4,29 +4,24 @@
 // We will then calculate the percent of the check and return
 // the amount for the user
 
+var resultText = document.getElementById("result")
+
 
 var billTotal = []
-var tipPercent = []
 
 // get the value off the input price box
 $("#calculate-total").on("click", function() {
     
     var inpVal = $("#price-input").val().trim()
-    var tipVal = $("#tip-percentage").val().trim()
 
     billTotal.push(inpVal)
-    tipPercent.push(tipVal)
 
+    var tipValue = document.getElementById("tipVal").value;
+    
+    var grandTotal = billTotal * tipValue
+    console.log(grandTotal)
 
-    
-    // we may have to use a switch statment based off the value clicked on the drop down.
-    
-    // so use this code: when the user picks the value of the drop down.
-    // excellent = .30%
-    // Great = .20%
-    // Good = .15%
-    // Terriable = .5%
-    
+    resultText.textContent = "$" + grandTotal
 
 })
 
